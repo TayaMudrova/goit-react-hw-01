4d7f0d5d639c4152cfcfd643509648827d50e1f0
Taya Mudrova <mudrova.taisiia@gmail.com>
2024-06-17 14:21:16 +0300

    second task done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import userData from "../../userData.json";
+import friends from "../../friends.json";
 
 import Profile from "../Profile/Profile";
+import FriendList from "../FriendList/FriendList";
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
         image={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 }
